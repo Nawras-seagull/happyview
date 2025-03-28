83de3f861dabacaeec8729015e9a4a63d0d5eb06
Nawras-seagull <nawrasmop@gmail.com>
2025-03-28 10:56:56 +0300

    fix download btn with gal package

---
+++ happyview3/happyview3/happyview3App.swift
@@ -0,0 +1,17 @@
+//
+//  happyview3App.swift
+//  happyview3
+//
+//  Created by Nawras Al Abbas on 26.03.2025.
+//
+
+import SwiftUI
+
+@main
+struct happyview3App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
