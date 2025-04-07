8e66a5ef19966125174a4543aecb06b9183723c2
Nawras-seagull <nawrasmop@gmail.com>
2025-04-07 23:16:16 +0300

    combine initstate in query_result and randomize image fetching

--- lib/screens/query_result.dart
+++ lib/screens/query_result.dart
@@ -1,4 +1,5 @@
 import 'dart:async';
+import 'dart:math';
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:cached_network_image/cached_network_image.dart';
@@ -15,13 +16,13 @@ class UnifiedPictureScreen extends StatefulWidget {
     super.key,
     this.query,
     this.initialResults,
-  }) : assert(query != null || initialResults != null,
-            'Either query or initialResults must be provided');
+  }) : assert(query != null || initialResults != null, 
+           'Either query or initialResults must be provided');
 
   @override
   UnifiedPictureScreenState createState() => UnifiedPictureScreenState();
 }
-
+////////////////////////
 class UnifiedPictureScreenState extends State<UnifiedPictureScreen> {
   final List<Map<String, dynamic>> _images = [];
   int _page = 1;
@@ -33,19 +34,20 @@ class UnifiedPictureScreenState extends State<UnifiedPictureScreen> {
 
   @override
   void initState() {
-    super.initState();
+  super.initState();
+
 
     // Set the current query if provided
-    if (widget.query != null) {
-      _currentQuery = widget.query!;
-    }
-
-    // Fetch images from the Unsplash API
-    _fetchImages();
-
-    // Add a listener to the scroll controller
-    _scrollController.addListener(_onScroll);
+  if (widget.query != null) {
+    _currentQuery = widget.query!;
   }
+
+  // Fetch images from the Unsplash API
+  _fetchImages();
+
+  // Add a listener to the scroll controller
+  _scrollController.addListener(_onScroll);
+}
 
   @override
   void dispose() {
@@ -54,11 +56,15 @@ class UnifiedPictureScreenState extends State<UnifiedPictureScreen> {
     super.dispose();
   }
 
-  Future<void> _fetchImages() async {
+  Future<void> _fetchImages({bool randomizePage = false}) async {
     if (_isLoading || !_hasMore) return;
 
     setState(() => _isLoading = true);
 
+        final random = Random();
+        _page =
+            random.nextInt(50) + 1; // Assuming Unsplash has at least 50 pages
+      
     try {
       final newImages = await UnsplashService.fetchImages(
         _currentQuery,
@@ -103,6 +109,7 @@ class UnifiedPictureScreenState extends State<UnifiedPictureScreen> {
   }
 
   void _openFullScreenImage(int index) {
+    
     Navigator.push(
       context,
       MaterialPageRoute(
@@ -111,6 +118,7 @@ class UnifiedPictureScreenState extends State<UnifiedPictureScreen> {
           photographerName: _images[index]['photographer'],
           photoLink: _images[index]['photoLink'],
           downloadUrl: _images[index]['download'], // Pass the download URL
+
         ),
       ),
     );
@@ -122,74 +130,74 @@ class UnifiedPictureScreenState extends State<UnifiedPictureScreen> {
       appBar: AppBar(
         title: Text(widget.query ?? 'Search Results'),
       ),
-      body: Stack(
-        // Changed from Column to Stack
+       body: Stack(  // Changed from Column to Stack
+      children: [
+        Column(
         children: [
-          Column(
-            children: [
-              Expanded(
-                child: _images.isEmpty && !_isLoading
-                    ? Center(
-                        child: Text(
-                          'No images found',
-                          style: TextStyle(
-                            fontSize: 18,
-                            color: Colors.grey[600],
+          Expanded(
+            child: _images.isEmpty && !_isLoading
+                ? Center(
+                    child: Text(
+                      'No images found',
+                      style: TextStyle(
+                        fontSize: 18,
+                        color: Colors.grey[600],
+                      ),
+                    ),
+                  )
+                : GridView.builder(
+                    controller: _scrollController,
+                    padding: const EdgeInsets.all(16.0),
+                    gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+                      crossAxisCount: 2,
+                      crossAxisSpacing: 16.0,
+                      mainAxisSpacing: 16.0,
+                      childAspectRatio: 0.7,
+                    ),
+                    itemCount: _images.length + (_hasMore ? 1 : 0),
+                    itemBuilder: (context, index) {
+                      if (index == _images.length) {
+                        return const Center(
+                          child: SpinKitThreeInOut(
+                            color: Color.fromARGB(255, 8, 127, 148),
+                            size: 30.0,
                           ),
-                        ),
-                      )
-                    : GridView.builder(
-                        controller: _scrollController,
-                        padding: const EdgeInsets.all(16.0),
-                        gridDelegate:
-                            const SliverGridDelegateWithFixedCrossAxisCount(
-                          crossAxisCount: 2,
-                          crossAxisSpacing: 16.0,
-                          mainAxisSpacing: 16.0,
-                          childAspectRatio: 0.7,
-                        ),
-                        itemCount: _images.length + (_hasMore ? 1 : 0),
-                        itemBuilder: (context, index) {
-                          if (index == _images.length) {
-                            return const Center(
-                              child: SpinKitThreeInOut(
-                                color: Color.fromARGB(255, 8, 127, 148),
-                                size: 30.0,
-                              ),
-                            );
-                          }
-                          final image = _images[index];
-                          return GestureDetector(
-                            onTap: () => _openFullScreenImage(index),
-                            child: ClipRRect(
-                              borderRadius: BorderRadius.circular(8.0),
-                              child: CachedNetworkImage(
-                                imageUrl: image['url'],
-                                fit: BoxFit.cover,
-                                placeholder: (context, url) => const Center(
-                                    child: SpinKitThreeInOut(
+                        );
+                      }
+                      final image = _images[index];
+                      return GestureDetector(
+                        onTap: () => _openFullScreenImage(index),
+                        child: ClipRRect(
+                          borderRadius: BorderRadius.circular(8.0),
+                          child: CachedNetworkImage(
+                            imageUrl: image['url'],
+                            fit: BoxFit.cover,
+                            placeholder: (context, url) =>
+                                const Center(child: SpinKitThreeInOut(
                                   color: Color.fromARGB(255, 8, 127, 148),
                                   size: 30.0,
                                 )),
-                                errorWidget: (context, url, error) =>
-                                    const Icon(Icons.error),
-                              ),
-                            ),
-                          );
-                        },
-                      ),
-              ),
-            ],
+                            errorWidget: (context, url, error) =>
+                                const Icon(Icons.error),
+                          ),
+                        ),
+                      );
+                    },
+                  ),
+                  
           ),
-          // Now the AnimatedPanda can be a direct child of Stack
-          const Positioned(
-            bottom: -1,
-            left: 0,
-            right: 0,
-            child: AnimatedPanda(),
-          ),
+     
         ],
       ),
-    );
-  }
+    // Now the AnimatedPanda can be a direct child of Stack
+         const Positioned(
+          bottom: -1,
+          left: 0,
+          right: 0, 
+          child: AnimatedPanda(),
+        ),
+      ],
+    ),
+  );
+}
 }
