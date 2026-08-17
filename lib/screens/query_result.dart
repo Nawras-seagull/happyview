b1e072dbfcc8c32246d7767e00e043eff43b8ff0
Nawras-seagull <seagull.nawras@gmail.com>
2026-08-17 12:13:37 +0300

    update Gradle, AGP, and Kotlin versions

--- lib/screens/query_result.dart
+++ lib/screens/query_result.dart
@@ -1,5 +1,4 @@
 import 'dart:async';
-import 'dart:math';
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:cached_network_image/cached_network_image.dart';
